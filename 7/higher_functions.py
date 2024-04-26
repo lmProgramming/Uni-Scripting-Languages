38fbd83eacd016b5a62c1a8cfbc3d25657bb9a25
lmProgramming <mikikubs@gmail.com>
2024-04-26 16:07:26 +0200

    finished higher funcs

--- 7/higher_functions.py
+++ 7/higher_functions.py
@@ -1,17 +1,3 @@
-'''
-Zaproponuj implementacje następujących funkcji wyższego rzędu przyjmujących
-predykat pred (unarną funkcję zwracającą wartość logiczną), iterable (obiekt, po
-którym można iterować) i potencjalnie n - dodatnią liczbę całkowitą.
-a. forall(pred, iterable) - funkcja zwraca True, jeśli każdy element
-iterable spełnia predykat pred, w przeciwnym przypadku False,
-b. exists(pred, iterable) – funkcja zwraca True, jeśli co najmniej jeden
-element iterable spełnia predykat pred, w przeciwnym przypadku False,
-c. atleast(n, pred, iterable) – funkcja zwraca True, jeśli co najmniej n
-elementów iterable spełnia predykat pred, w przeciwnym przypadku False.
-d. atmost(n, pred, iterable) – funkcja zwraca True, jeśli co najwyżej n
-elementów iterable spełnia predykat pred, w przeciwnym przypadku False.
-'''
-
 def forall(pred, iterable):
     for element in iterable:
         if not pred(element):
