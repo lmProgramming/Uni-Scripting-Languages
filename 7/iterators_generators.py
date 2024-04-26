cf78a8b5ea8d49f15b789b847978e2826e2a20df
lmProgramming <mikikubs@gmail.com>
2024-04-26 16:05:48 +0200

    finished iterators generators

--- 7/iterators_generators.py
+++ 7/iterators_generators.py
@@ -30,14 +30,30 @@ def atleast(n, pred, iterable):
         if pred(element):
             count += 1
             if count >= n:
+                return False
+    return True
+
+def atmost(n, pred, iterable):
+    count = 0
+    for element in iterable:
+        if pred(element):
+            count += 1
+            if count > n:
                 return True
     return False
 
+print("forall")
 print(forall(lambda x: x > 0, [1, 2, 3, 4, 5]))
 print(forall(lambda x: x > 0, [1, 2, 3, -4, 5]))
 
+print("exists")
 print(exists(lambda x: x < 0, [1, 2, 3, 4, 5]))
 print(exists(lambda x: x < 0, [1, 2, 3, -4, 5]))
 
+print("atleast")
 print(atleast(5, lambda x: x > 0, [1, 2, 3, 4, 5]))
 print(atleast(5, lambda x: x > 0, [1, 2, 3, -4, 5]))
+
+print("atmost")
+print(atmost(4, lambda x: x > 0, [1, 2, 3, 4, 5]))
+print(atmost(4, lambda x: x > 0, [1, 2, 3, -4, 5]))
