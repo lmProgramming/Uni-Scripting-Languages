f246a3501bf4c15a5997b5169f55e16ac0d243e5
lmProgramming <mikikubs@gmail.com>
2024-04-30 07:33:11 +0200

    removed comment

--- 6/ssh_user.py
+++ 6/ssh_user.py
@@ -1,19 +1,6 @@
 from datetime import datetime
 import re
 
-'''
-a. Utwórz klasę SSHUser reprezentującą użytkownika. Niech klasa pozwala na
-przechowywanie informacji o nazwie użytkownika i dacie ostatniego logowania.
-Zdefiniuj w niej metodę validate() , która będzie walidować poprawność nazwy4
-użytkownika.
-b. Następnie pobierz kilka instancji SSHLogEntry z SSHLogJournal. Utwórz kilka
-instancji klasy SSHUser. Przechowaj wszystkie instancje na wspólnej liście.
-c. Zademonstruj działanie kaczego typowania poprzez iterację po tej liście i
-wywoływanie metody validate() na obiektach różnego typu.
-4 np.przypomocywyrażeniaregularnegor'^[a-z_][a-z0-9_-]{0,31}$'
-3
-'''
-
 class SSHUser:
     username: str
     last_login: datetime
